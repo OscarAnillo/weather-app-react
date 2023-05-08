80af381acfa5571381f5221547e028da65652942
Oscar Anillo <oscarani14@gmail.com>
2023-05-08 17:55:22 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Oscar Anillo</h1>
+      <p>Fullstack Developer</p>
+    </div>
+  )
+}
+
+export default App
